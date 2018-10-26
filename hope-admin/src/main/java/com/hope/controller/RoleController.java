fc928287fab580ec20feb82dbebdaf9277cbdf4e
java-aodeng <35907367+java-aodeng@users.noreply.github.com>
2018-10-26 16:51:21 +0800

    提交版本：0.2.7 说明：提交部分代码

--- hope-admin/src/main/java/com/hope/controller/RoleController.java
+++ hope-admin/src/main/java/com/hope/controller/RoleController.java
@@ -11,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -44,10 +45,15 @@ public class RoleController {
         return ResultHopeUtil.tablePage(pageInfo);
     }
 
-/*    @RequiresPermissions("roles")
-    @PostMapping("/list")
-    public PageResult getAll(RoleConditionVO vo) {
-        PageInfo<Role> pageInfo = roleService.findPageBreakByCondition(vo);
-        return ResultUtil.tablePage(pageInfo);
-    }*/
+    @GetMapping("/add")
+    public ModelAndView add(Model model){
+        log.info("[role-add-page]-[{}]","测试210");
+        return ResultHopeUtil.view("admin/role/add");
+    }
+
+    @GetMapping("/edit")
+    public ModelAndView edit(Model model){
+        log.info("[role-edit-page]-[{}]","测试220");
+        return ResultHopeUtil.view("admin/role/edit");
+    }
 }

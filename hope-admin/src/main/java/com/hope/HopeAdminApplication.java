0a9df0b964a16f945f30a9e09ee9d453b9b34ef4
qq18281714982 <java@aodeng.cc>
2019-02-25 21:46:30 +0800

    :fire: 优化代码

--- hope-admin/src/main/java/com/hope/HopeAdminApplication.java
+++ hope-admin/src/main/java/com/hope/HopeAdminApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HopeAdminApplication {
-
+    
     public static void main(String[] args) {
         SpringApplication.run(HopeAdminApplication.class, args);
+
     }
 }
--- hope-framework/src/main/java/com/hope/utils/Md5HopeUtil.java
+++ hope-framework/src/main/java/com/hope/utils/Md5HopeUtil.java
@@ -69,10 +69,12 @@ public class Md5HopeUtil {
             StringBuffer buf = new StringBuffer("");
             for (int offset = 0; offset < b.length; offset++) {
                 i = b[offset];
-                if (i < 0)
+                if (i < 0) {
                     i += 256;
-                if (i < 16)
+                }
+                if (i < 16) {
                     buf.append("0");
+                }
                 buf.append(Integer.toHexString(i));
             }
             str = buf.toString();

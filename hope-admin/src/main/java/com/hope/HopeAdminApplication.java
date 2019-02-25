ce79f8a26b5172d5b67f1c5d88bd78e731fc1dc8
qq18281714982 <java@aodeng.cc>
2019-02-25 21:47:13 +0800

    :fire: 优化代码

--- hope-admin/src/main/java/com/hope/HopeAdminApplication.java
+++ hope-admin/src/main/java/com/hope/HopeAdminApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HopeAdminApplication {
-    
+
     public static void main(String[] args) {
         SpringApplication.run(HopeAdminApplication.class, args);
-
     }
 }

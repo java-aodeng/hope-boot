80b8ebf2e41a5649b9d81acc9c1e9289346fe459
java-aodeng <java@aodeng.cc>
2019-04-01 13:11:46 +0800

    :art: 搭建sso认证中心 v0.7

---
+++ hope-sso-server/src/main/java/com/hope/model/beans/UserInfo.java
@@ -0,0 +1,20 @@
+package com.hope.model.beans;
+
+import lombok.Data;
+
+/**
+ * @program:hope-plus
+ * @ClassName:UserInfo
+ * @author:aodeng
+ * @blog:低调小熊猫(https://aodeng.cc)
+ * @create:2019-04-01 13:08
+ * @Description: TODO
+ * @Version 1.0
+ **/
+@Data
+public class UserInfo {
+
+    private int userId;
+    private String userName;
+    private String passWord;
+}

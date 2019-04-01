3c6b81ba15af83c8b9b66d4a7dc28db6ebf8bf28
java-aodeng <java@aodeng.cc>
2019-04-01 15:30:32 +0800

    :art: sso server ok

--- hope-sso-server/src/main/java/com/hope/HopeSsoServerApplication.java
+++ hope-sso-server/src/main/java/com/hope/HopeSsoServerApplication.java
@@ -5,8 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class HopeSsoServerApplication {
 
 	/** logger */

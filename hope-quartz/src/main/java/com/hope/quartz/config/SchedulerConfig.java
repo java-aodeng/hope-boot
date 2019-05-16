d6a86b216177da382a0650cc589c9564060095c1
java-aodeng <java@aodeng.cc>
2019-05-16 13:01:27 +0800

    :art: 0.0.4 描述：完善quartz job 注入bean，规范日志

---
+++ hope-quartz/src/main/java/com/hope/quartz/config/SchedulerConfig.java
@@ -0,0 +1,32 @@
+package com.hope.quartz.config;
+
+import org.quartz.Scheduler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.quartz.SchedulerFactoryBean;
+
+/**
+ * @author:aodeng(低调小熊猫)
+ * @blog:（https://aodeng.cc)
+ * @Description: TODO
+ * @Date: 19-5-16
+ **/
+@Configuration
+public class SchedulerConfig {
+
+    @Autowired
+    private SpringJobFactory springJobFactory;
+
+    @Bean
+    public SchedulerFactoryBean schedulerFactoryBean() {
+        SchedulerFactoryBean schedulerFactoryBean = new SchedulerFactoryBean();
+        schedulerFactoryBean.setJobFactory(springJobFactory);
+        return schedulerFactoryBean;
+    }
+
+    @Bean
+    public Scheduler scheduler() {
+        return schedulerFactoryBean().getScheduler();
+    }
+}
